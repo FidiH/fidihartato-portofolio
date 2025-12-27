be4d0b52ecbe92776e365c17a81772f1937e868c
root <root@localhost.localdomain>
2025-12-27 04:34:20 +0000

    fix(content): modif konten

--- src/lib/components/portofolio/Section_Tentang.svelte
+++ src/lib/components/portofolio/Section_Tentang.svelte
@@ -32,27 +32,23 @@
 		<article class="my-5">
 			<h3 class="geser mb-2 text-lg font-semibold">singkat</h3>
 			<p class="geser pb-2">
-				Saya siswa kelas 11 di smk aaa, mengambil jurusan teknik jaringan komputer dan teknologi, namun saya memilih jurusan tersebut bukan karena minat melainkan karena penasaran pada jaringan.
+				Saya siswa kelas 11 di smk LPPM RI 2 KEDUNGREJA, mengambil jurusan teknik jaringan komputer dan teknologi, namun saya memilih jurusan tersebut bukan karena minat melainkan karena penasaran pada jaringan.
 			</p>
 			<p class="geser pb-2">
-				Disisi lain saya juga mencari hal-hal lain, bisa di katakan hampir 95% hal-hal yang saya tahu berawal dari belajar otodidak dan learning path lewat internet.
+				Saya adalah seorang Autodidact Learner. Sekitar 95% pengetahuan teknis yang saya miliki saat ini adalah hasil dari eksplorasi mandiri melalui internet, mulai dari membedah video YouTube, membaca artikel teknis, hingga berdiskusi intens dengan AI untuk menyusun modul belajar yang sistematis.
 			</p>
 		</article>
 		<hr class="geser" />
 		<article class="my-5">
-			<h3 class="geser mb-2 text-lg font-semibold">Jalur belajar (Pendidikan)</h3>
-			<p class="geser pb-2">Saya percaya bahwa ilmu tersebar luas dan gratis di internet bagi siapa pun yang mau mencarinya.
-			</p>
-			<p class="geser pb-2">Kalau ada yang bikin saya penasaran, saya langsung search tutorialnya di YouTube. Kalau video kurang detail, saya ulik di artikel yang bertebaran.
-			</p>
-			<p class="geser pb-2">Di era sekarang, saya nggak cuma baca. Saya gabungin referensi dari YouTube dan artikel tadi buat dijadiin bahan diskusi bareng AI. Saya bikin prompt sendiri supaya bisa dapet modul belajar yang pas dan sistematis buat saya pahami.
+			<h3 class="geser mb-2 text-lg font-semibold">Filosofi Belajar</h3>
+			<p class="geser pb-2">Saya percaya Bagi saya, ilmu itu gratis dan tersebar luas. Batasannya hanyalah seberapa jauh kita mau mencari. Saya tidak hanya mengonsumsi konten, tapi menyaring dan merakitnya menjadi pemahaman yang utuh.
 			</p>
 		</article>
 		<hr class="geser" />
 		<article class="my-5">
 			<h3 class="geser mb-2 text-lg font-semibold">Soft Skills</h3>
 			<p class="geser">
-Karena terbiasa melakukan hal sendiri mulai dari menyiap modul dan sumber ini membuat saya memilih pola pikir yang terorganisir agar memudahkan proses, Meskipun saya suka belajar sendiri, saya tetep bisa diajak kerja bareng dalam tim.
+        Karena terbiasa melakukan hal sendiri mulai dari menyiap modul dan sumber ini membuat saya memilih pola pikir yang terorganisir agar memudahkan proses, Meskipun saya suka belajar sendiri, saya tetep bisa diajak kerja bareng dalam tim.
 			</p>
 		</article>
 	</div>
