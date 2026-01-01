1aee58272396f9d636f9e34093fd6cecf8853ef8
fidi hartato <fidihartato.pidi@gmail.com>
2026-01-01 06:46:50 +0000

    fixhot: meta tag dan compress img

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,40 @@
 <script>
-	// import css
-	import './layout.css';
+  // import css
+  import './layout.css';
+  
+  // 
+  import icon from "$lib/assets/pp.webp"
+  import graph from "$lib/assets/graph.webp"
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
 <svelte:head>
-	<!--head seperti html-->
-	<title>Fidi Hartato | Siswa SMK | Web Learner
-</title>
-	<meta name="description" content="portofolio fidi hartato, eksplore hal dan tampilkan" />
-	<meta
-		name="keywords"
-		content="portofolio, smk lppm ri 2 kedungreja, siswa, fidi, fidi hartato, fidi smk"
-	/>
-	<meta name="author" content="Fidi Hartato" />
+  <!--head seperti html-->
+  <link rel="icon" type="image/x-icon" href={icon}>
+  <title>Fidi Hartato | Portofolio</title>
+  <meta name="title" content="fidihartato | Portofolio Siswa TJKT SMK LPPM RI 2 Kedungreja">
+  <meta name="description" content="Saya adalah siswa kelas XI SMK LPPM RI 2 Kedungreja, jurusan Teknik Jaringan Komputer dan Telekomunikasi. Selamat datang di portofolio digital saya.">
+  <meta name="keywords" content="fidi, fidi hartato, fidi smk, Portofolio fidi, fidi lppm ri 2, TJKT, SMK LPPM RI 2 Kedungreja">
+  <meta name="author" content="Fidi Hartato">
+  <meta name="robots" content="index, follow">
+  <meta name="language" content="Indonesian">
+  <meta name="geo.region" content="ID">
+  <meta name="revisit-after" content="7 days">
+
+  <link rel="canonical" href="https://fidihartato.my.id/">
+  <meta property="og:type" content="website">
+  
+  <meta property="og:url" content="https://fidihartato.my.id/"> <meta property="og:title" content="fidihartato | Portofolio">
+  <meta property="og:description" content="Saya adalah siswa kelas XI SMK LPPM RI 2 Kedungreja, jurusan Teknik Jaringan Komputer dan Telekomunikasi.">
+  <meta property="og:image" content={graph}> <meta property="twitter:card" content="summary_large_image">
+  <meta property="twitter:url" content="https://fidihartato.my.id/">
+  <meta property="twitter:title" content="fidihartato | Portofolio Siswa">
+  <meta property="twitter:description" content="Saya adalah siswa kelas XI SMK LPPM RI 2 Kedungreja, jurusan Teknik Jaringan Komputer dan Telekomunikasi.">
+  <meta property="twitter:image" content={graph}>
+
+  <meta name="theme-color" content="#2faaff">
+
 </svelte:head>
 
 <!--merender semua yang dikirimkan page.svelte-->
